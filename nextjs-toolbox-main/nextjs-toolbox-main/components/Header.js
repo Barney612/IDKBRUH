6e7c99192de031734047e489203dbbaa01452b5b
Barney612 <148951477+Barney612@users.noreply.github.com>
2023-10-26 11:38:11 -0700

    Add files via upload

---
+++ nextjs-toolbox-main/nextjs-toolbox-main/components/Header.js
@@ -0,0 +1,10 @@
+export default function Header({ title }) {
+  return (
+    <>
+      <h1 className="title">{title}</h1>
+      <p>
+        Please fill out the information below, and all the secrets to a healthy life will be revealed.
+      </p>
+    </>
+  );
+}
